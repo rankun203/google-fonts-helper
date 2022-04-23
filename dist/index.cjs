08a649005f0c284633ed076c70f89d48838b8d32
Kun Ran <rankun203@gmail.com>
2022-04-23 23:33:03 +0800

    fix: add ttf support

--- dist/index.cjs
+++ dist/index.cjs
@@ -265,7 +265,7 @@ class Downloader extends hookable.Hookable {
       stylePath: "fonts.css",
       fontsDir: "fonts",
       fontsPath: "./fonts",
-      headers: [options?.ttf ? [""] : ["user-agent", [
+      headers: [options?.ttf ? ["user-agent curl"] : ["user-agent", [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
         "AppleWebKit/537.36 (KHTML, like Gecko)",
         "Chrome/98.0.4758.102 Safari/537.36"
